2008a818c11e8456ea718190632d815c8c5a6216
Alexander de Oliveira <c1311597@bb.com.br>
2020-04-06 18:31:58 -0300

    wip

--- src/components/Match.js
+++ src/components/Match.js
@@ -7,7 +7,8 @@ import {
     Image,
     Dimensions
 } from 'react-native'
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
+// import { AnimatedCircularProgress } from 'react-native-circular-progress'
+import ProgressBar from 'react-native-progress/Bar'
 
 class Match extends Component {
     render() {
@@ -16,9 +17,7 @@ class Match extends Component {
         let image2 = { uri: this.props.team2ImageURL }
         return (
             <View style={styles.container}>
-                <View style={ { flexDirection: 'row', alignContent: 'center', justifyContent: 'center' } } >
-                </View>
-                <AnimatedCircularProgress
+                {/* <AnimatedCircularProgress
                     size={140}
                     width={2}
                     fill={50}
@@ -37,10 +36,31 @@ class Match extends Component {
                         )
                         
                     }
-                    </AnimatedCircularProgress>
+                </AnimatedCircularProgress> */}
+                <ProgressBar 
+                    progress={0.5} 
+                    width={100}
+                    height={1}
+                    borderWidth = {0}
+                    color={'#22D48D'}
+                    unfilledColor= {'#707070'}  />
+
+                <View style={ { flex:1, margin: 15, alignItems: 'center' } } >
+                    <View style={ { flexDirection: 'row' } }   >
+                        <Image source={image1} style={styles.image}/>
+                        <Text style={ styles.textScore }  > {this.props.team1Score} </Text>
+                        <Text style={ styles.textScore }  > {this.props.team2Score} </Text>
+                        <Image source={image2} style={styles.image}/>
+                    </View>
+                    <View style={ { flexDirection: 'row', alignContent: 'center', justifyContent: 'center', marginTop: 5 } }   >
+                        <Text style={ styles.textModality }  > {this.props.modality} </Text>  
+                        <Text style={ styles.textModality }  >-</Text>  
+                        <Text style={ styles.textModality }  > {this.props.gender} </Text>  
+
+                    </View>
+                </View>
+
                 
-                {/* <Text>{this.props.team1}</Text>
-                <Text>{this.props.team2}</Text> */}
             </View>
         )
     }
@@ -49,16 +69,28 @@ class Match extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 10,
-        flexDirection: 'row',
+        margin: 10,
         alignItems: 'center',
-        width: '100%'
     },
     image: {
         width: 40,
         height: 40,
         resizeMode: 'contain'
+    },
+    textScore: {
+        fontSize: 20,
+        alignSelf: 'center',
+        fontFamily: 'Overpass-Regular',
+        color: '#fff'
+    },
+    textModality: {
+        fontSize: 10,
+        fontFamily: 'Overpass-Regular',
+        color: '#fff',
+        alignContent: 'center'
     }
+
+
 })
 
 export default Match
--- src/screens/Matches.js
+++ src/screens/Matches.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 import HeaderHome from '../components/HeaderHome'
-import  {StyleSheet, FlatList, View } from 'react-native'
+import  {StyleSheet, FlatList, View, Text, Image } from 'react-native'
 import Match from '../components/Match'
 import { fetchMatches } from '../store/actions/matchesAction'
 import { connect } from 'react-redux'
 import { Alert } from 'react-native'
+import arrowLeft from '../../assets/images/arrow_left.png'
+import arrowRigth from '../../assets/images/arrow_rigth.png'
+
+
 
 class Matches extends Component {
     componentDidMount = () => {
@@ -15,8 +19,16 @@ class Matches extends Component {
         return (
             <View style={styles.container}>
                 <HeaderHome/>
+                <View style={ { flexDirection: 'row', margin: 20, justifyContent: 'space-between' } } >
+                    <Text style={ styles.textTitle } > Jogos ao vivo </Text>
+                    <View style={ { flexDirection: 'row' } } >
+                        <Image source={ arrowLeft } style={ styles.imageLeft } />
+                        <Image source={ arrowRigth } style={ styles.imageRigth } />
+                    </View>
+                </View>
+                
                 <FlatList
-                    // horizontal={true}
+                    horizontal={true}
                     data={this.props.matches}
                     keyExtractor={item => `${item.id}`}
                     renderItem={({ item }) => 
@@ -29,10 +41,29 @@ class Matches extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#16191D'
-    }
+    },
+    imageLeft: {
+        width: 20,
+        height: 20,
+        resizeMode: 'contain'
+    },
+    imageRigth: {
+        width: 20,
+        height: 20,
+        marginLeft: 5,
+        resizeMode: 'contain'
+    },
+    textTitle: {
+        fontSize: 20,
+        fontFamily: 'Overpass-Regular',
+        color: '#fff',
+    },
+    text: {
+        fontSize: 20,
+        fontFamily: 'Overpass-Regular',
+        color: '#fff',
+    },
 })
 
 const mapStateToProps = ({ matches }) => {
